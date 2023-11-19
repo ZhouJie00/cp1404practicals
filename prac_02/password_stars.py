528d2b9e193130ba46da1f5d7223b9ea74cf01c2
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2023-11-19 20:33:09 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,20 +1,28 @@
-
-
 MINI_PASS_LENGTH = 10
 
 
-
 def main():
-    password = input("please enter a password: ")
+    password = get_password()
     while len(password) < MINI_PASS_LENGTH:
         print("Please enter a password with a minimum length of 10 characters")
-        password = input("please enter a password: ")
+        password = get_password()
 
+    asterisk_password = get_asterisk(password)
+    print(asterisk_password)
+
+
+def get_password():  # get password
+    password = input("please enter a password: ")
+
+    return password
+
+
+def get_asterisk(password):  # get asterisk
     asterisk_password = ""
     for i in range(len(password)):
         asterisk_password += "*"
 
-    print(asterisk_password)
+    return asterisk_password
 
-main()
 
+main
